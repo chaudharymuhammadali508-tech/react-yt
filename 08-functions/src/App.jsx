0b8586b2f6b327f4317945d12a5c298c75452186
Chaudhary Muhammad Ali <chaudharymuhammadali508@gmail.com>
2025-10-27 00:25:03 +0500

    i uploaded function folder

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+      function pageScrolling(elem){
+           if(elem>0){
+        console.log("scrolling down");}
+        else{
+          console.log("scrolling up");
+          
+        }
+      }
+    
+  return (
+    <div onWheel={(elem)=>{
+        pageScrolling(elem.deltaY)
+    }}>
+    <div className='page1'></div>
+    <div className='page2'></div>
+     <div className='page3'> </div>
+    </div>
+  )
+}
+
+export default App
